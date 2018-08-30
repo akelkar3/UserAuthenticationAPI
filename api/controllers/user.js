48fb60902dd86b3b8fd0ad7ba648e0fdcf756c29
Snigdha Bose <sbose1@uncc.edu>
2018-08-30 15:15:35 -0400

    modified response json

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -83,6 +83,7 @@ exports.user_login = (req, res, next) => {
           return res.status(200).json({
             message: "Auth successful",
             token: token,
+            userId:user[0]._id,
             status: 200
           });
         }
